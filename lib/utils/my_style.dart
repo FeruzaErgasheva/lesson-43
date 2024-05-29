9ca3f0fa8d1c012bc3e8fe75994240a861d85df7
Feruza <ergashevaferuzabonuu@gmail.com>
2024-05-29 07:54:45 +0500

    first commit

---
+++ lib/utils/my_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static ThemeMode themeMode = ThemeMode.light;
+  static String imageUrl =
+      'https://t4.ftcdn.net/jpg/03/92/25/09/360_F_392250914_2Od8jNRBPgpMu8W29vCh4hiu5EUXbgGU.jpg';
+  static String language = 'UZB';
+  static Color appColor = Colors.deepOrange;
+
+  static String password = '2006';
+  static Color textColor = Colors.blue;
+  static double textSize = 14;
+}
